8e94d07610d514ffbe8c6c7a6fed61c40ac001bf
yohanpannet <yohan.pannet@gmail.com>
2017-10-05 15:21:14 +0200

    rappeler code projet, 1ere version

--- spec/rappelerCodeProjet.test.js
+++ spec/rappelerCodeProjet.test.js
@@ -1,7 +1,15 @@
-require("jasmine");
+require('jasmine');
+var index = require('../src/index.js');
 
-describe("Rappeler code projet", function(){
-    it("retourne code ", function(){
-        expect(false).toBeTruthy();
+
+describe('Rappeler code projet', function(){
+    it('repond ', function(){
+        var bot = {
+            reply: function(message, texte) {}
+        };
+        spyOn(bot, 'reply').and.stub();
+        var message = {};
+        index.rappelerCodeProjet(bot, message);
+        expect(bot.reply).toHaveBeenCalledWith(message, 'Proxilabs-affaire 1700643');
     })
 });
